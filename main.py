c9c215b35f4a3b456cad7bd79b7ef858a5c0e4f4
un-nova <lobss21@gmail.com>
2022-12-29 18:44:58 +0300

    лобанова светлана: окно загрузки

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
-import random
-import os
 import pygame
+import time
+import cv2
 
-item_dic = {"rrr": 5, "melody": 10, "ggg": 14, "ggg": 10, "ggg": 30}
-times = 1
-characters = []
-c = []
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
+pygame.display.set_caption('HelloKitty Garden')
 
+
+def cv2ImageToSurface(cv2Image):
+    size = cv2Image.shape[1::-1]
+    format = 'RGBA' if cv2Image.shape[2] == 4 else 'RGB'
+    cv2Image[:, :, [0, 2]] = cv2Image[:, :, [2, 0]]
+    surface = pygame.image.frombuffer(cv2Image.flatten(), size, format)
+    return surface.convert_alpha() if format == 'RGBA' else surface.convert()
+
+
+def loadGIF(filename):
+    gif = cv2.VideoCapture(filename)
+    frames = []
+    while True:
+        ret, cv2Image = gif.read()
+        if not ret:
+            break
+        pygameImage = cv2ImageToSurface(cv2Image)
+        frames.append(pygameImage)
+    return frames
+
+
+pygame.init()
+window = pygame.display.set_mode((650, 500))
 clock = pygame.time.Clock()
 
+gifFrameList = loadGIF(r"pix.gif")
+currentFrame = 0
+pygame.init()
 
-def gacha(item_dic, times):
-    total_weight = 0
-    for value in item_dic.values():
-        total_weight += value
-
-    results = []
-    for i in range(times):
-        results.append(lottery(item_dic, total_weight))
-
-    return results
-
-
-def lottery(item_dic, total_weight):
-    score = random.randint(1, total_weight)
-    range_max = 0
-    for item_key, weight in item_dic.items():
-        range_max += weight
-        if score <= range_max:
-            return item_key
-
-
-item_list = gacha(item_dic, times)
-f = (" ".join(item_list))
-if f == 'ggg':
-    image = pygame.image.load("curs.png")
-    screen.blit(image, (0, 0))
-
-if item_list not in c:
-    c.append(str(item_list))
-print(c)
-
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+run = True
+while run:
+    clock.tick(7)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mopos = pygame.mouse.get_pos()
